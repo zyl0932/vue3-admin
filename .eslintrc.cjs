1c7755386478787d922f765e82384e379589b30e
zyl0932 <646092224@qq.com>
2023-10-20 17:08:17 +0800

    feat: 初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        // 环境，针对哪些环境的语法
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        // 集成了哪些规则，别人写好的规则直接拿来用
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended", // typescript 规则
+        "plugin:vue/vue3-essential",
+        "@vue/prettier"
+    ],
+    "overrides": [],
+    "parser": "vue-eslint-parser", // 解析.vue文件
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser", // 解析.ts文件
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+        // 我们自定义的规则写在这里
+        "prettier/prettier": [
+            {
+                singleQuote: false, // 使用单引号
+                semi: false, // 末尾添加分号
+                tabWidth: 2,
+                trailingComma: "none",
+                useTabs: false,
+                endOfLine: "auto"
+            }
+        ]
+    }
+}
